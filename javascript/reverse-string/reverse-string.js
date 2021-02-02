75d4cee779a8532148326c83939a3b51167bd54f
Paul Livingstone <36920884+TheFlyer1983@users.noreply.github.com>
2021-02-02 19:46:48 +0000

    reverse-string (#23)

---
+++ javascript/reverse-string/reverse-string.js
@@ -0,0 +1,14 @@
+//
+// This is only a SKELETON file for the 'Reverse String' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+/**
+ * 
+ * @param {string} str
+ * @return {string} 
+ */
+
+export const reverseString = (str) => {
+  return str.split('').reverse().join('');
+};
